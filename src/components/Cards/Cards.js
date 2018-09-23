e0891b572e347b12e036a8a8559311d0314f2f9e
max-allen <contact@maxallen.io>
2018-09-23 14:37:21 -0400

    CardGrid and Card Components

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -1,38 +1,53 @@
 import React, { Component } from 'react'
 import styles from './Cards.scss'
 
-const Card = (props) => {
-  const { data, showing } = props
-
-  return (
-    <div className={styles.container}>
-      {
-        data.map((card, idx) => {
-          return (
-            <div key={idx} className={styles.card}>
-              {showing ? card : '' }
-            </div>
-          )
-        })
-      }
-
-    </div>
-  )
-
-}
-
-class CardContainer extends Component {
+class Card extends Component {
   constructor(props) {
     super()
+    this.state = { showing: false }
 
-    this.state = {
-      showing: false,
+    this.cardRef = React.createRef();
+    this.handleClick = this.handleClick.bind(this)
+  }
+
+  componentDidMount() {
+    document.addEventListener('mousedown', this.handleClick)
+  }
+
+  handleClick(e) {
+    const { showing } = this.state
+
+    if (this.cardRef.current.contains(e.target)) {
+      this.setState({ showing: !showing })
     }
   }
 
   render() {
-    return <Card data={this.props.data} showing={this.state.showing} />
+    const { data } = this.props
+    const { showing } = this.state
+    const { cardRef } = this
+
+    return (
+      <div className={styles.card} ref={cardRef}>
+        {showing ? data : ''}
+      </div>
+    )
   }
 }
 
-export default CardContainer
+
+const CardGrid = (props) => {
+  const { data } = props
+
+  return (
+    <div className={styles.container}>
+        {
+          data.map((card, idx) => {
+            return <Card key={idx} data={card} />
+          })
+        }
+    </div>
+  )
+}
+
+export default CardGrid
