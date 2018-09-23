2428dee8d29d4c19378b3185b069b9d8c133a7cc
max-allen <contact@maxallen.io>
2018-09-23 16:17:40 -0400

    add selected cards to Game state via props callback. check for matching cards in cDU. reset game state given outcome.

--- src/components/Cards/Cards.js
+++ src/components/Cards/Cards.js
@@ -4,46 +4,63 @@ import styles from './Cards.scss'
 class Card extends Component {
   constructor(props) {
     super()
-    this.state = { showing: false }
+    this.state = { showing: false, selected: false }
 
-    this.cardRef = React.createRef();
+    this.cardRef = React.createRef()
     this.handleClick = this.handleClick.bind(this)
+    this.chooseCard = this.chooseCard.bind(this)
   }
 
   componentDidMount() {
     document.addEventListener('mousedown', this.handleClick)
   }
 
-  handleClick(e) {
-    const { showing } = this.state
+  componentDidUpdate(prevProps) {
+    const { resetTurn } = this.props
 
-    if (this.cardRef.current.contains(e.target)) {
-      this.setState({ showing: !showing })
+    if (prevProps.resetTurn !== resetTurn) {
+      if (resetTurn) {
+        this.setState({ showing: false, selected: false })
+      }
     }
   }
 
+  handleClick(e) {
+    const { showing, selected } = this.state
+
+    if (!selected) {
+      if (this.cardRef.current.contains(e.target)) {
+        this.setState({ showing: !showing })
+      }
+    }
+  }
+
+  chooseCard(card) {
+    this.setState({ selected: true })
+    this.props.updateSelected(card)
+  }
+
   render() {
-    const { data } = this.props
+    const { id, data, updateSelected } = this.props
     const { showing } = this.state
-    const { cardRef } = this
+    const { cardRef, chooseCard } = this
 
     return (
-      <div className={styles.card} ref={cardRef}>
+      <div className={styles.card} ref={cardRef} onClick={() => chooseCard({ [id]: data })}>
         {showing ? data : ''}
       </div>
     )
   }
 }
 
-
 const CardGrid = (props) => {
-  const { data } = props
+  const { data, updateSelected, resetTurn } = props
 
   return (
     <div className={styles.container}>
         {
           data.map((card, idx) => {
-            return <Card key={idx} data={card} />
+            return <Card key={idx} id={idx} data={card} updateSelected={updateSelected} resetTurn={resetTurn} />
           })
         }
     </div>
--- src/components/Game/Game.js
+++ src/components/Game/Game.js
@@ -13,7 +13,12 @@ export default class Game extends Component {
     this.state = {
       setting: 'easy',
       cards: {},
+      selectedCards: {},
+      removedCards: {},
+      resetTurn: false,
     }
+
+    this.updateSelected = this.updateSelected.bind(this)
   }
 
   componentDidMount() {
@@ -23,14 +28,51 @@ export default class Game extends Component {
       .then(data => this.setState({ cards: data.levels }))
   }
 
+  componentDidUpdate() {
+    let { selectedCards, resetTurn } = this.state
+    let { removedCards } = this.state
+
+    if (resetTurn) this.setState({ resetTurn: false })
+
+    const cardKeys = Object.keys(selectedCards)
+    const cardValues = Object.values(selectedCards)
+
+    if (cardValues.length === 2) {
+      if (cardValues[0] === cardValues[1]) {
+        removedCards = Object.assign({}, removedCards, selectedCards)
+        selectedCards = {}
+
+        this.setState({ removedCards, selectedCards })
+      } else {
+        selectedCards = {}
+        resetTurn = true
+
+        this.setState({ selectedCards, resetTurn })
+      }
+    }
+  }
+
+  updateSelected(card) {
+    const { selectedCards } = this.state
+    const ids = Object.keys(selectedCards)
+
+    const nextId = Object.keys(card)[0]
+
+    if (!ids.includes(nextId)) {
+      selectedCards[nextId] = card[nextId]
+
+      this.setState({ selectedCards })
+    }
+  }
+
   render() {
-    const { cards, setting } = this.state
+    const { cards, setting, removedCards, resetTurn } = this.state
 
     return (
       <div>
         <Navbar />
         {this.state.cards.length && (
-          <Cards data={cards.filter(level => level.difficulty === setting)[0].cards} />
+          <Cards data={cards.filter(level => level.difficulty === setting)[0].cards} updateSelected={this.updateSelected} resetTurn={resetTurn} />
         )}
       </div>
     )
